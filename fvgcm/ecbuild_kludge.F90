f907a35c3543fa3073305e2d69a66d53ad6a66fa
Tom Clune <Tom.Clune@nasa.gov>
2019-06-18 20:32:20 -0400

    Kludeg for using ecbuild.
    CMake now supports declaring targets with no sources (but with sources
    added in later), but ecbuild does not.  Or at least does not directly.

---
+++ fvgcm/ecbuild_kludge.F90
@@ -0,0 +1,6 @@
+! ecbuild_add_library() does not support empty source list.  Actually,
+! it might, but if so, another argument needs to be passed down from
+! the local CMakeLists.txt file.
+
+subroutine ecbuild_kludge()
+end subroutine ecbuild_kludge
